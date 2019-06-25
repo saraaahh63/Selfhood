8ba2550c04841432844270e7d176ae2fc747cbfa
saraaahh63 <sat454@nyu.edu>
2019-06-25 17:13:14 -0400

    upload

---
+++ Website/mainwebsite/script.js
@@ -0,0 +1,402 @@
+function Scroll() {
+     window.scroll(120,0); 
+}
+
+window.onload = Scroll;
+
+function popup(mylink, popup)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup", 'width=300,height=50');
+return false;
+}
+
+function popup1(mylink, popup1)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup1", 'width=500,height=400,scrollbars=yes');
+return false;
+}
+
+function popup2(mylink, popup2)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup2", 'width=400,height=300, left=500, top=350');
+return false;
+}
+
+
+function popup4(mylink, popup4)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup4", 'width=300,height=100, left=500, top=100');
+return false;
+}
+
+function popup5(mylink, popup5)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup5", 'width=300,height=50, left=700, top=200');
+return false;
+}
+
+function popup6(mylink, popup6)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup6", 'width=1000,height=300, left=200, top=100');
+return false;
+}
+
+function popup7(mylink, popup7)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup7", 'width=300,height=50');
+return false;
+}
+
+function popup8(mylink, popup8)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup8", 'width=600,height=400, left=200, top=100');
+return false;
+}
+
+function popup9(mylink, popup9)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup9", 'width=500,height=400, left=700, top=200');
+return false;
+}
+
+function popup10(mylink, popup10)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup10", 'width=400,height=50, left=500, top=500');
+return false;
+}
+
+function popup11(mylink, popup11)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup11", 'width=300,height=50');
+return false;
+}
+
+function popup12(mylink, popup12)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup12", 'width=500,height=400, left=400, top=100');
+return false;
+}
+
+function popup13(mylink, popup13)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup13", 'width=250,height=250, left=200, top=100');
+return false;
+}
+
+function popup14(mylink, popup14)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup14", 'width=354,height=264, top=300, left=100');
+return false;
+}
+
+function popup15(mylink, popup15)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup15", 'width=300,height=50, left=700, top=200');
+return false;
+}
+
+function popup16(mylink, popup16)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup16", 'width=400,height=350, left=700, top=200');
+return false;
+}
+
+function popup17(mylink, popup17)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup17", 'width=500,height=450, top=100');
+return false;
+}
+
+function popup18(mylink, popup18)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup18", 'width=400,height=350, left=700, top=100');
+return false;
+}
+
+function popup19(mylink, popup19)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup19", 'width=300,height=50');
+return false;
+}
+
+function popup20(mylink, popup20)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup20", 'width=500,height=450, left=200, top=100');
+return false;
+}
+
+function popup21(mylink, popup21)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup21", 'width=400,height=50, left=500, top=500');
+return false;
+}
+
+function popup22(mylink, popup22)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup22", 'width=300,height=50');
+return false;
+}
+
+function popup23(mylink, popup23)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup23", 'width=400,height=350, left=700, top=100');
+return false;
+}
+
+function popup24(mylink, popup24)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup24", 'width=600,height=334, left=400');
+return false;
+}
+
+function popup25(mylink, popup25)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup25", 'width=600,height=400');
+return false;
+}
+
+function popup26(mylink, popup26)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup26", 'width=400,height=350, left=200, top=100');
+return false;
+}
+
+function popup27(mylink, popup27)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup27", 'width=400,height=350, left=100, top=400');
+return false;
+}
+
+function popup29(mylink, popup29)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup29", 'width=300,height=50');
+return false;
+}
+
+function popup28(mylink, popup28)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup28", 'width=400,height=330');
+return false;
+}
+
+function popup31(mylink, popup31)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup31", 'width=500,height=450, left=400, top=100');
+return false;
+}
+
+function popup30(mylink, popup30)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup30", 'width=323,height=499, left=800');
+return false;
+}
+
+function popup33(mylink, popup33)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup33", 'width=700,height=50, left=500, top=100');
+return false;
+}
+
+function popup32(mylink, popup32)
+{
+if (! window.focus)return true;
+var href;
+if (typeof(mylink) == 'string')
+   href=mylink;
+else
+   href=mylink.href;
+window.open(href, "popup32", 'width=500,height=50, left=700, top=200');
+return false;
+}
